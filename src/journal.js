7f7cfcb050d99992c3c5093a52b4b1d964a1114e
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-01 14:53:06 -0800

    set up environment for project
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

---
+++ src/journal.js
@@ -0,0 +1,40 @@
+export default function Entry(title, body) {
+  this.title = title;
+  this.body = body;
+}
+
+Entry.prototype.getNumWords = function() {
+  let words = this.body.split(" ");
+  let wordCount = words.length();
+  return wordCount;
+};
+
+Entry.prototype.getNumVowels = function() {
+  let letters = this.body.split("");
+  let vowelList = ["a", "e", "i", "o", "u", "y"];
+  let vowelCount = 0;
+  let consonantCount = 0;
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i] === vowelList) {
+      vowelCount++;
+    } else if (letters[i] !== vowelList) {
+      consonantCount++;
+    }
+  }
+  let results = [vowelCount, consonantCount];
+  return results;
+};
+
+Entry.prototype.getTeaser = function() {
+  let words = this.body.split(" ");
+  let firstWords = [];
+  if (words.length < 8) {  
+    for (let i = 0; i < words.length; i++) {
+      firstWords = words[i];
+    } 
+  } else {      
+    firstWords = words.slice(0,8);
+  }
+  firstWords = firstWords.join(" ");
+  return firstWords;
+};
---
+++ src/main.js
@@ -0,0 +1,25 @@
+//business logic ----------
+import Entry from './journal.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import $ from 'jquery';
+
+
+
+//UI logic ---------------
+$(document).ready(function() {
+  $("journal-form").submit(function(event) {
+    event.PreventDefault();
+    const title = $("#title").val();
+    const body = $("#body").val();
+    const entry = new Entry(title, body);
+    const teaser = entry.getTeaser();
+    const wordCount = entry.getNumWords();
+    const consVowels = entry.getNumVowels();
+    $("#teaser").text(teaser);
+    $("#word-count").text(wordCount);
+    $("#vowel-count").text(consVowels[0]);
+    $("#consonant-count").text(consVowels[1]);
+  });
+});
